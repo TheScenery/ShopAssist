1f43bab9163bc8a6ce2af20b6d744424c68481c0
Wenbin Liu <liuwenbin.lwb@gmail.com>
2023-05-11 21:40:39 +0800

    UI basic layout

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,11 +1,14 @@
 import './assets/main.css'
+import 'element-plus/dist/index.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementPlus from 'element-plus'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(ElementPlus)
 
 app.mount('#app')
