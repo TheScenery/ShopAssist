861dd87847d2d1504335b2259641663c972b30de
TheScenery <liuwenbin.lwb@gmail.com>
2023-08-31 22:20:18 +0800

    try use slf4j

--- back-end/src/main/java/io/thescenery/shopAssist/auth/controller/AuthController.java
+++ back-end/src/main/java/io/thescenery/shopAssist/auth/controller/AuthController.java
@@ -6,9 +6,11 @@ import io.thescenery.shopAssist.auth.service.IAuthService;
 import io.thescenery.shopAssist.auth.service.ITokenService;
 import io.thescenery.shopAssist.shared.security.IPasswordService;
 import io.thescenery.shopAssist.user.entity.User;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@Slf4j
 public class AuthController {
     private final IPasswordService passwordService;
     private final IAuthService authService;
@@ -30,7 +32,8 @@ public class AuthController {
     public LoginResponseDto login(@RequestBody LoginRequestDto loginData) {
         User user = authService.loginWithPassword(loginData);
         if (user != null) {
-           return LoginResponseDto.builder().token(tokenService.getToken(user.getId())).build();
+            log.info("User login, userID: {}", user.getId());
+            return LoginResponseDto.builder().token(tokenService.getToken(user.getId())).build();
         }
         return null;
     }
