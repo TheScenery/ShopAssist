8e5969106c34fa8a0509afa7476d212d29b06ae4
TheScenery <liuwenbin.lwb@gmail.com>
2023-09-19 23:49:23 +0800

    feat: add create app dialog
    Signed-off-by: TheScenery <liuwenbin.lwb@gmail.com>

---
+++ front-end/src/components/dialog/ModalDialog.vue
@@ -0,0 +1,36 @@
+<script setup>
+
+defineProps({
+  modelValue: Boolean,
+  title: String
+});
+const emit = defineEmits(['update:modelValue', 'onOk']);
+
+const onClose = () => {
+  emit('update:modelValue', false);
+};
+
+const onConfirm = () => {
+  emit('onOk');
+  onClose();
+};
+
+</script>
+
+<template>
+  <el-dialog :visivle='modelValue' :title='title'>
+    <slot></slot>
+    <template #footer>
+      <span class='dialog-footer'>
+        <el-button @click='onClose'>{{ $t('cancel') }}</el-button>
+        <el-button type='primary' @click='onConfirm'>
+          {{ $t('confirm') }}
+        </el-button>
+      </span>
+    </template>
+  </el-dialog>
+</template>
+
+<style scoped lang='scss'>
+
+</style>
--- front-end/src/views/workspace-view/WorkspaceView.vue
+++ front-end/src/views/workspace-view/WorkspaceView.vue
@@ -2,18 +2,30 @@
 import { Edit } from '@element-plus/icons-vue';
 import { onMounted, ref } from 'vue';
 import { getMyApps } from '@/api/app';
+import ModalDialog from '@/components/dialog/ModalDialog.vue';
 
 const apps = ref([]);
+const createDialogOpen = ref(false);
 
 onMounted(async () => {
   apps.value = await getMyApps();
 });
 
+const openCreateAppDialog = () => {
+  createDialogOpen.value = true;
+};
+
+const createApp = () => {
+  console.log('create app');
+};
+
 </script>
 
 <template>
   <div class='workspaces-container'>
-    <el-button type='primary' :icon='Edit' class='create-workspace-button'>{{ $t('app.create') }}</el-button>
+    <el-button type='primary' :icon='Edit' class='create-workspace-button' @click='openCreateAppDialog'>{{ $t(
+      'app.create') }}
+    </el-button>
     <div class='workspaces-list-container'>
       <el-space wrap size='large'>
         <el-card v-for='workspace in apps' :key='workspace.id' class='workspace-container'>
@@ -21,6 +33,9 @@ onMounted(async () => {
         </el-card>
       </el-space>
     </div>
+    <ModalDialog v-model='createDialogOpen' @onOk='createApp' :title='$t("app.create")'>
+      This is create app dialog
+    </ModalDialog>
   </div>
 </template>
 
